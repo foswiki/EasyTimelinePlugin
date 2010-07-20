51781d972456ccb83e0a73d9ffa5feb628078c9e
DanDascalescu <DanDascalescu@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-07-20 22:35:53 +0000

    Item8841: use strict for EasyTimelinePlugin_convert.pl
    git-svn-id: http://svn.foswiki.org/trunk/EasyTimelinePlugin@8259 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- tools/EasyTimelinePlugin_convert.pl
+++ tools/EasyTimelinePlugin_convert.pl
@@ -1,17 +1,19 @@
-#usr/bin/perl -w
+#!/usr/bin/perl -w
 
 # copy this script into your data directory and it will loop through all topics and convert the old EasyTimelinePlugin syntax to the new Foswiki one
 # While this worked for me, I can not guarentee it will work for you.
 # Always test on a copy of your data first!
 
+use strict;
 use File::Find::Rule;
 
 my @subdirs = File::Find::Rule->directory->in( '.' );
 
-foreach $dir (@subdirs) {
+foreach my $dir (@subdirs) {
     next if ($dir =~ m/^_|^\.|^Trash$/);
     # open dir, loop through files, open files with .txt$, search for syntax, output if found
     opendir( DIR, $dir) or die "can't opendir: $!";
+    my $filename;
     while( defined ( $filename = readdir ( DIR ) ) ) {
         if( $filename =~ /.*\.txt$/ ) {
 
